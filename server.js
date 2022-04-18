5cb5e82fa3e6cd206d62b7617ca0fc8ac97f523c
Terry-404 <gamecorner71@gmail.com>
2022-04-19 01:41:24 +0200

    Adding: body-Parser module and setting it up

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+//used as a reference to order the shortened url's
+let shortenedNum = 1
+// Shortened links registered
+let shortened = {
+  '1': "https://github.com/Terry-404"
+}
+
 app.use(cors());
+app.use(bodyParser.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +27,7 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
